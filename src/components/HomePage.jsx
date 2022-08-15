44929d9461f9d42d4b242f5aee2c8fc8caa316b1
starydv7 <pawanstar72926@gmail.com>
2022-08-15 23:02:16 +0530

    early setup

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+
+const HomePage = () => {
+  return (
+    <div>HomePage</div>
+  )
+}
+
+export default HomePage
